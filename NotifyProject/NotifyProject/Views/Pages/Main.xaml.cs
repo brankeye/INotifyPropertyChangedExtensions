5101f47fca2b2845d24a74b8ab9ea95852131055
Brandon Keyes <brankeye@gmail.com>
2016-07-16 18:27:18 -0400

    Initial setup.

---
+++ NotifyProject/NotifyProject/Views/Pages/Main.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NotifyProject.Views
+{
+    public partial class Main : ContentPage
+    {
+        public ViewModels.Pages.Main ViewModel => _vm ?? (_vm = BindingContext as ViewModels.Pages.Main);
+        private ViewModels.Pages.Main _vm;
+
+        public Main()
+        {
+            InitializeComponent();
+            BindingContext = new ViewModels.Pages.Main();
+        }
+    }
+}
